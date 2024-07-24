219e377e8375f3b2a622354a91e47f05fe6f0304
Kasim <mermer@advasco.com>
2024-07-24 16:14:37 +0200

    Create class for entitiy: Country

---
+++ app/src/main/java/com/example/countries/entity/Country.kt
@@ -0,0 +1,5 @@
+package com.example.countries.entity
+
+class Country(private var code: String, private var currencyCodes: String, private var name: String, private var wikiDataId: String, private var saved: Boolean = false) {
+
+}
