2b3d55c8e76b01c3e3ea32352a32a34b2cc8f927
Kasim <mermer@advasco.com>
2024-07-25 21:11:40 +0200

    update adapter to show saved items in the saved fragment

--- app/src/main/java/com/example/countries/adapter/CountryAdapter.kt
+++ app/src/main/java/com/example/countries/adapter/CountryAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.countries.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -8,9 +9,14 @@ import com.example.countries.R
 import com.example.countries.databinding.TemplateListItemBinding
 import com.example.countries.entity.Country
 
-class CountryAdapter(
-    private val countries: MutableList<Country>
-) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+class CountryAdapter(private val countriesList : MutableList<Country>, private val onlySaved: Boolean): RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+    private var filteredCountriesList: List<Country> = if(onlySaved){
+        countriesList.filter{
+            it.saved
+        }
+    }else {
+        countriesList
+    }
 
     class CountryViewHolder(val binding: TemplateListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -23,7 +29,8 @@ class CountryAdapter(
         return CountryViewHolder(binding)
     }
 
-    private fun toggleSaveCountry(ivSaveCountry: ImageView, currentCountry: Country){
+    @SuppressLint("NotifyDataSetChanged")
+    private fun toggleSaveCountry(ivSaveCountry: ImageView, currentCountry: Country) {
         if(!currentCountry.saved){
             ivSaveCountry.setImageResource(R.drawable.saved_icon)
             currentCountry.saved = true
@@ -31,21 +38,35 @@ class CountryAdapter(
             ivSaveCountry.setImageResource(R.drawable.unsaved_icon)
             currentCountry.saved = false
         }
+
+        filteredCountriesList = if(onlySaved){
+            countriesList.filter{
+                it.saved
+            }
+        }else{
+            countriesList
+        }
+        notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        val currentCountry = countries[position]
+        val currentCountry = filteredCountriesList[position]
         holder.binding.apply {
             tvCountryName.text = currentCountry.name
-
+            ivSaveCountry.setImageResource(
+                if(currentCountry.saved) {
+                    R.drawable.saved_icon
+                }else{
+                    R.drawable.unsaved_icon
+                }
+            )
             ivSaveCountry.setOnClickListener{
                 toggleSaveCountry(ivSaveCountry, currentCountry)
             }
-
         }
     }
 
     override fun getItemCount(): Int {
-        return countries.size
+        return filteredCountriesList.size
     }
 }
